b28ab60b787ca46ab2ccaca0719a9698973f1597
razor1997 <webmastering1997@gmail.com>
2022-04-12 22:50:02 +0200

    Added new form for repairs

---
+++ src/app/car-repair/car-repair/car-repair.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-car-repair',
+  templateUrl: './car-repair.component.html',
+  styleUrls: ['./car-repair.component.css']
+})
+export class CarRepairComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
